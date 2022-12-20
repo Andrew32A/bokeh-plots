718dbc1eaccb1fe8f9974e8d41988c893e8f9fca
Andrew Alsing <111094607+Andrew32A@users.noreply.github.com>
2022-12-19 19:43:34 -0800

    added bar graphs

---
+++ bar graphs/bar_graphs.py
@@ -0,0 +1,43 @@
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.palettes import Spectral3
+from bokeh.transform import factor_cmap
+import random
+
+# sample data
+fruits = ["Apples", "Pears", "Kiwis", "Plums", "Bananas", "Strawberries"]
+years = ["2020", "2021", "2022"]
+
+# randomizes sample data
+data_2020 = random.sample(range(1, 10), len(fruits))
+data_2021 = random.sample(range(1, 10), len(fruits))
+data_2022 = random.sample(range(1, 10), len(fruits))
+
+# fruit and years dict
+data = {"fruits": fruits, "2020": data_2020, "2021": data_2021, "2022": data_2022}
+
+fruit_year = [(fruit, year) for fruit in fruits for year in years]
+counts = sum(zip(data["2020"], data["2021"], data["2022"]), ())
+
+df = dict(fruit_year = fruit_year, counts = counts)
+source = ColumnDataSource(data=df)
+
+tools = "box_zoom, undo, crosshair, lasso_select"
+
+p = figure(x_range = FactorRange(*fruit_year), height = 250, title = "Fruit Counts by Year", toolbar_location = None, tools = tools)
+
+p.vbar(x = "fruit_year", top = "counts", width = 0.9, source = source, fill_color = factor_cmap("fruit_year", palette = Spectral3, factors = years, start = 1, end = 2))
+
+
+p.y_range.start = 0
+
+# range padding
+p.x_range.factor_padding = 0.1
+
+# tilt
+p.xaxis.major_label_orientation = 1
+
+# gets rid of grid line color
+p.xgrid.grid_line_color = None
+
+show(p)
